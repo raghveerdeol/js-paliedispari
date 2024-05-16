5d5f2309e4697e7b084fd4250a605e9c9472f431
raghveerdeol <deolraghveer5@gmail.com>
2024-05-16 16:46:31 +0200

    creata funzione con al interno ciclo while  per comparare le lettere della stringa

--- js/script.js
+++ js/script.js
@@ -1,46 +1,35 @@
 
 
-// 3 nella funzione devo paragonare la prima lettera della parola (che chiameremo x) con l'ultima lettera (che chiameremo n);
-
-;
-
-
-
-// 9 nella condizione if parola[x] === parola[n]{
-//     allora la parola è palidroma;
-// } else {
-//     la parola non è palidroma
-// } 
-// 10 inseriamo il risultato nella variabile vuota
-// 11 return variabile. 
-
 // 1 chiedere input parola al utente;
 const userWord = prompt('Inserire una parola');
 
 // 2 aprire funzione;
 function palidroma(word) {
     // 4 dichiariamo x = 0;
-    const x = 0;
+    let x = 0;
     // 5 dichiariamo n = parola.length - 1
-    const n = word.length - 1;
+    let n = word.length - 1;
     // 6 dichiariamo una variabile vuota in cui inserire il risultato;
     let risultato = 0;
     // 7 apriamo un ciclo while con condizione x = n;
-    while (x === n) {
+    let preRisultato = false;
+    while (x !== n && x < n) {
         // 8 nel ciclo while inseriamo un if;
-        if (word[x] === word[n]) {
-            risultato = "La parola è palidroma";
-        } else {
-            risultato = "La parola non è palidroma";
+        if (word[x] !== word[n]) {
+            preRisultato = true;
         }
         x = x + 1;
         n = n - 1;
-        console.log(word[x], word[n])
+    }
+    if (preRisultato) {
+        risultato = "La parola non è palidroma";
+    } else{
+        risultato = "La parola è palidroma";
     }
     return risultato; 
     
 }
 
-palidroma(userWord);
+let stampa = palidroma(userWord);
 
-console.log(palidroma(userWord));
+console.log(stampa);
