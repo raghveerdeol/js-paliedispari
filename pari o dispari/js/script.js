7642bcbdb9abf5ae7873c8895590465671019648
raghveerdeol <deolraghveer5@gmail.com>
2024-05-16 21:01:35 +0200

    aggiunto userNumber e computerNumber

--- pari o dispari/js/script.js
+++ pari o dispari/js/script.js
@@ -1,12 +1,26 @@
 
 
-// 3 nella funzione generiamo un numero random da 1 a 5;
+
 // 4 in un altra funzione sommiamo userNumer + computerNumber;
 // 5 sempre nella funzione controlliamo se la somma è pari o dispari;
 // 6 confrontiamo il risultato della seconda funzione con la scelta del utente;
 // 7 se coincide ha vinto l'utente se no vince il  computer;
 
 // 1 input scelta utente in una costante tra pari o dispari;
-const scelta = prompt("pari o dispari?");
+let scelta = prompt("pari o dispari?");
+/* if (scelta === '' || scelta !== 'dispari' || scelta !== 'pari' ) {
+    scelta = 'pari';
+} */
 // 2 input numero compreso tra 1 e 5;
-const userNumer = Number.parseInt(prompt("Inserire un numero compreso tra 1 e 5"), 10)
+let userNumer = Number.parseInt(prompt("Inserire un numero compreso tra 1 e 5"), 10);
+
+if (Number.isNaN(userNumer) || userNumer > 5 || userNumer < 1){
+    userNumer = Math.floor(Math.random() * 5) + 1; 
+}
+let computerNumber = randomNumber();
+// 3 nella funzione generiamo un numero random da 1 a 5;
+function randomNumber() {
+    return Math.floor(Math.random() * 5) + 1; 
+}
+
+console.log(userNumer, scelta, computerNumber);
