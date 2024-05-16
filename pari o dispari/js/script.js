febcc7729503dc6f3b57015367d2102d0b578d22
raghveerdeol <deolraghveer5@gmail.com>
2024-05-16 21:17:45 +0200

    creata la seconda funzione e confrontato il risultato con l'input del utente

--- pari o dispari/js/script.js
+++ pari o dispari/js/script.js
@@ -1,11 +1,5 @@
 
 
-
-// 4 in un altra funzione sommiamo userNumer + computerNumber;
-// 5 sempre nella funzione controlliamo se la somma è pari o dispari;
-// 6 confrontiamo il risultato della seconda funzione con la scelta del utente;
-// 7 se coincide ha vinto l'utente se no vince il  computer;
-
 // 1 input scelta utente in una costante tra pari o dispari;
 let scelta = prompt("pari o dispari?");
 /* if (scelta === '' || scelta !== 'dispari' || scelta !== 'pari' ) {
@@ -22,5 +16,27 @@ let computerNumber = randomNumber();
 function randomNumber() {
     return Math.floor(Math.random() * 5) + 1; 
 }
+// 4 in un altra funzione sommiamo userNumer + computerNumber;
+function sum(giocatore1, giocatore2) {
+    somma = giocatore1 + giocatore2;
+    let risultato = 0;
+    // 5 sempre nella funzione controlliamo se la somma è pari o dispari;
+    if (somma % 2 === 0) {
+        risultato = "pari";
+    } else {
+        risultato = 'dispari';
+    }
+    return risultato;
+}
 
-console.log(userNumer, scelta, computerNumber);
+let totale = sum(userNumer, computerNumber);
+// 6 confrontiamo il risultato della seconda funzione con la scelta del utente;
+// 7 se coincide ha vinto l'utente se no vince il  computer;
+if (scelta === totale) {
+    console.log("Hai Vinto");
+} else {
+    console.log('Ha vinto il computer');
+}
+
+
+console.log(userNumer, scelta, computerNumber, totale);
